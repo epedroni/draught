18de871c945273f37dfc6587610918d64705db7a
Eduardo Pedroni <e.pedroni91@gmail.com>
2015-01-03 20:25:54 +0100

    Added publish feature and some help documentation

---
+++ manager.py
@@ -0,0 +1,18 @@
+import os
+
+class Manager(object):
+	def __init__(self, path):
+		# declare fields
+		self.fileList = []
+		
+		# create list of managed files
+		if os.path.isdir(path):
+			index = 1
+			for root, dirnames, filenames in os.walk(path):
+				for filename in filenames:
+					self.fileList.append([filename, root])
+		else:
+			raise Exception(path + " is not a valid path")
+	
+	def getContents(self):
+		return self.fileList
